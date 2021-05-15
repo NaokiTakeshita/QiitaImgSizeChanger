2ff136a3181477b3b02d00d3e047d3fc630c1def
Naoki Takeshita <n.takeshita.x@gmail.com>
2021-05-15 19:51:15 +0900

    del vc

--- qiitaImgSizeChanger/AppDelegate.swift
+++ qiitaImgSizeChanger/AppDelegate.swift
@@ -299,6 +299,7 @@ extension NSNotification.Name {
 }
 
 
+// releaseではprintコマンドを無視
 func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
     #if DEBUG
         let output = items.map { "\($0)" }.joined(separator: separator)
